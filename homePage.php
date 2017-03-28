b9636c1e33ab1ff35c062f359d53432d0ab19060
Michael Sanchez <michaelsanchez.0011@gmail.com>
2017-03-28 19:58:04 +0000

    Fixed the option details

--- homePage.php
+++ homePage.php
@@ -16,46 +16,48 @@
     </head>
     <body>
         <h1>Welcome to the Football Store!</h1>
-        <form action='' method = 'post'>
-            <select name = 'sortMethod'>
+        <div id = 'option'>
+            <form action='' method = 'post'>
+                <select name = 'sortMethod'>
+                    <?php
+                        if($_SESSION['sortOption'] == ''){
+                            $_SESSION['sortOption'] = 'Sort By';
+                            echo '<option>';
+                            echo $_SESSION['sortOption'];
+                            echo '</option>'; 
+                        }
+                        else{
+                            echo '<option>';
+                            echo $_SESSION['sortOption'];
+                            echo '</option>';
+                        }
+                        for($i = 0; $i < count($sortingOptions); $i++){
+                            if($sortingOptions[$i] != $_SESSION['sortOption']){
+                                echo "<option>";
+                                echo $sortingOptions[$i];
+                                echo "</option>";
+                            }
+                        }
+                    ?>
+                </select>
                 <?php
-                    if($_SESSION['sortOption'] == ''){
-                        $_SESSION['sortOption'] = 'Sort By';
-                        echo '<option>';
-                        echo $_SESSION['sortOption'];
-                        echo '</option>'; 
+                    if($_SESSION['orderType'] == 'ASC'){
+                        echo "<label>Ascending<input type='radio' name='orderType' value='ASC' checked/></label>";
+                        echo "<label>Descending<input type='radio' name='orderType' value='DESC'/></label>";
+                    }
+                    else if($_SESSION['orderType'] == 'DESC'){
+                        echo "<label>Ascending<input type='radio' name='orderType' value='ASC'/></label>";
+                        echo "<label>Descending<input type='radio' name='orderType' value='DESC' checked/></label>";
                     }
                     else{
-                        echo '<option>';
-                        echo $_SESSION['sortOption'];
-                        echo '</option>';
-                    }
-                    for($i = 0; $i < count($sortingOptions); $i++){
-                        if($sortingOptions[$i] != $_SESSION['sortOption']){
-                            echo "<option>";
-                            echo $sortingOptions[$i];
-                            echo "</option>";
-                        }
+                        echo "<label>Ascending<input type='radio' name='orderType' value='ASC'/></label>";
+                        echo "<label>Descending<input type='radio' name='orderType' value='DESC'/></label>";
                     }
                 ?>
-            </select>
-            <?php
-                if($_SESSION['orderType'] == 'ASC'){
-                    echo "<label>Ascending<input type='radio' name='orderType' value='ASC' checked/></label>";
-                    echo "<label>Descending<input type='radio' name='orderType' value='DESC'/></label>";
-                }
-                else if($_SESSION['orderType'] == 'DESC'){
-                    echo "<label>Ascending<input type='radio' name='orderType' value='ASC'/></label>";
-                    echo "<label>Descending<input type='radio' name='orderType' value='DESC' checked/></label>";
-                }
-                else{
-                    echo "<label>Ascending<input type='radio' name='orderType' value='ASC'/></label>";
-                    echo "<label>Descending<input type='radio' name='orderType' value='DESC'/></label>";
-                }
-            ?>
-                
-            <input type="submit" name="filterOption"/>
-        </form>
+                    
+                <input type="submit" name="filterOption"/>
+            </form>
+        </div>
         <?php
             $query = 'SELECT Player.playerName, Team.teamName FROM Player LEFT JOIN Team ON 
                       Team.teamId = Player.teamId';
