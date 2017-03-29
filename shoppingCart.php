a73a2907668e7d0d619d203dd564b7e9f230c159
Samuel Sanchez <sasanchez@csumb.edu>
2017-03-29 03:25:53 +0000

    Added more Front End Stuff. A lot more.

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,9 +1,38 @@
+<?php
+    session_start();
+    require("conn.php");
+    
+    $_SESSION['sortOption'] = $_POST['sortMethod'];
+    $_SESSION['orderType'] = $_POST['orderType'];
+    $_SESSION['selections'] = $_POST['selection'];
+    $sortingOptions = ['Sort By', 'Team','Position','Name'];
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <title>Shopping Cart</title>
     </head>
     <body>
-
+        <h1>My Shopping Cart</h1>
+        <div class = 'returnButtonContainer'>
+            <form action = 'jerseyPage.php' method='post'>
+                <button class='returnButton'>Return To Jersey Page</button>
+            </form>
+            <form action = 'gameTickets.php' method='post'>
+                <button class='returnButton'>Return To Ticket Page</button>
+            </form>
+        </div>
+        <?php
+            echo "<table>";
+            for($i = 0; $i < count($_SESSION['selections']); $i++){
+                if($_SESSION['selections'][$i] != '0'){
+                    echo "<tr><td>" . $_SESSION['selections'][$i] . "</td></tr>";
+                }
+            }
+            echo "</table>";
+        ?>
     </body>
 </html>
--- updateSearch.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $_SESSION['teamFilter'] = $_POST[];
-?>
